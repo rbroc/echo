68294390faa4b125fb9276feaeb4c62d4a31ef6f
rbroc <rbrrcc@gmail.com>
2023-06-29 12:42:34 +0200

    add descriptives notebook for dailydialog

---
+++ data/dailydialog/mappings.py
@@ -0,0 +1,13 @@
+emo_dict = {0: 'none', 
+            1: 'anger', 
+            2: 'disgust', 
+            3: 'fear', 
+            4: 'happiness', 
+            5: 'sadness', 
+            6: 'surprise'}
+
+act_dict = {0: 'dummy', 
+            1: 'inform', 
+            2: 'question', 
+            3: 'directive',
+            4: 'commissive'}
