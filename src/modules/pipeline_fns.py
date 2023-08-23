b967fd7c183b196048769c89e48306f87f01a50c
MinaAlmasi <minaalmasi00@gmail.com>
2023-08-23 11:02:45 +0200

    add beluga prompt formatting

--- src/modules/pipeline_fns.py
+++ src/modules/pipeline_fns.py
@@ -34,6 +34,10 @@ def load_file(filepath):
     return df 
 
 def create_prompt(df, datafile:str="dailymail_cnn", prompt_number:int=1): 
+    '''
+    Create prompt column. Tested on models FLAN-T5 (xl), Falcon 7B (instruct and normal).   
+    '''
+
     prompts = {
         # daily mail (summarization)
         "dailymail_cnn_1": "summarize the main points of this article: ", 
@@ -63,6 +67,62 @@ def create_prompt(df, datafile:str="dailymail_cnn", prompt_number:int=1):
     df[f"prompt_{prompt_number}"] = prompts[f"{datafile}_{prompt_number}"] + df["source"].copy()
     
     return df 
+
+def create_beluga_prompt(df, datafile:str="dailymail_cnn", prompt_number:int=1):
+    '''
+    Create prompt column for Stable Beluga 7B 
+    (the model follows a specific prompt structure: https://huggingface.co/stabilityai/StableBeluga-7B) 
+    '''
+
+    # define system prompt 
+    system_prompt = "### System:\nYou are StableBeluga, an AI that follows instructions extremely well. Help as much as you can. Remember, be safe, and don't do anything illegal.\n\n"
+
+    # task prompt (specify task)
+    task_prompts = {
+        # daily mail (summarization)
+        "dailymail_cnn_1": "summarize the main points of this article: ", 
+        "dailymail_cnn_2": "create a summary of the news article: ", 
+        "dailymail_cnn_3": "write a short summarised text of the news article: ",
+        "dailymail_cnn_4": "summarize this: ",
+
+        # stories (text generate)
+        "stories_1": "continue the story: ",
+        "stories_2": "write a small text based on this story: ",
+        "stories_3": "complete the text: ",
+        "stories_4": "complete the story: ",
+
+        # mrpc (paraphrase)
+        "mrpc_1": "paraphrase this text: ",
+        "mrpc_2": "summarize this text: ", 
+        "mrpc_3": "summarize this: ",
+        "mrpc_4": "create a summary of this: ",
+
+        # dailydialog 
+        "dailydialog_1": "respond to the final sentence: ",
+        "dailydialog_2": "continue this dialog: ",
+        "dailydialog_3": "finish this dialog: ",
+        "dailydialog_4": "respond to the final sentence: "   }
+
+    # empty list for formatted 
+    formatted_prompts = []
+
+    for row in df.itertuples(): 
+        # exract source text
+        source_text = row.source
+
+        # create prompt
+        user_prompt = task_prompts[f"{datafile}_{prompt_number}"] + source_text
+
+        # formatted, final prompt
+        final_prompt = f"{system_prompt}### User: {user_prompt}\n\n### Assistant:\n"
+
+        # append to list 
+        formatted_prompts.append(final_prompt)
+
+    df[f"prompt_{prompt_number}"] = formatted_prompts
+
+    return df 
+
 
 def completions_generator(df, prompt_col:str, model, model_name:str, min_len:int , max_tokens: int, outfilepath=None):
     '''
