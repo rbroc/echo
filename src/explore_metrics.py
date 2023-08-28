2bf008e2ce69830c134385c97bcbd3137b65f501
MinaAlmasi <minaalmasi00@gmail.com>
2023-08-28 14:24:35 +0200

    add quantiles

--- src/explore_metrics.py
+++ src/explore_metrics.py
@@ -47,13 +47,29 @@ def get_median_doc_length(datapath, filename):
     completions = pd.read_csv(completions_file)
 
     # get dictionary
-    mean_lengths = {
+    median_lengths = {
         f"{filename}_source": round(source["doc_length"].median(), 3),
         f"{filename}_completions": round(completions["doc_length"].median(), 3)
     }
 
-    return mean_lengths
+    return median_lengths
 
+def get_quantiles(datapath, filename):
+    # define paths
+    source_file = datapath / f"{filename}_source.csv"
+    completions_file = datapath / f"{filename}_completions.csv"
+
+    # load files
+    source = pd.read_csv(source_file)
+    completions = pd.read_csv(completions_file)
+
+    # get dictionary
+    quantile_lengths = {
+        f"{filename}_source": source["doc_length"].quantile([0.25, 0.50, 0.75]),
+        f"{filename}_completions": completions["doc_length"].quantile([0.25, 0.50, 0.75])
+    }
+
+    return quantile_lengths
 
 def main(): 
     # init args
@@ -65,15 +81,19 @@ def main():
 
     mean_lengths = []
     median_lengths = []
+    quantile_lengths = []
 
     for filename in ["stories", "dailydialog", "dailymail_cnn", "mrpc"]:
         mean_len = get_mean_doc_length(datapath, filename)
         median_len = get_median_doc_length(datapath, filename)
+        quantile_len = get_quantiles(datapath, filename)
 
         mean_lengths.append(mean_len)
         median_lengths.append(median_len)
+        quantile_lengths.append(quantile_len)
 
     print(f"\n MEAN lengths for each dataset \n {mean_lengths} \n\n MEDIAN lengths for each dataset \n {median_lengths}")
+    print(f"\n QUANTILES \n {quantile_lengths}")
 
 if __name__ == "__main__":
     main()
