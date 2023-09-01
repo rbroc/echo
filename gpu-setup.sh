9deafc8379c59aff5a7bb846ee6270ffe15a9594
MinaAlmasi <minaalmasi00@gmail.com>
2023-09-01 11:31:40 +0200

    add venv to gpu setup

--- gpu-setup.sh
+++ gpu-setup.sh
@@ -8,5 +8,6 @@ echo -e "[INFO:] Updating GPU drivers ..."
 sudo apt install nvidia-driver-525 nvidia-utils-525 -y
 
 echo -e "[INFO:] Installing python-venv... Rebooting after this step. \n You'll need to reconnect to the virtual machine using the ssh command."
+sudo apt install python3.8-venv
 
 sudo reboot
