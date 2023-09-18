4bc551a6f8961a1450f883a74fece5aaf86c1723
MinaAlmasi <minaalmasi00@gmail.com>
2023-09-18 11:52:15 +0200

    add ai metrics script

---
+++ run_ai.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+source ./env/bin/activate
+
+# run models and datasets sequentially
+datasets=("dailydialog" "dailymail_cnn" "mrpc") # stories should also be there, but currently left out
+models=("beluga" "falcon_instruct" "llama2_chat")
+
+for dataset in "${datasets[@]}"
+do
+    for model in "${models[@]}"
+    do
+        echo "Processing dataset: $dataset with model: $model"
+        python src/extract_metrics_ai.py --dataset "$dataset" --model "$model"
+    done
+done
+
+deactivate
