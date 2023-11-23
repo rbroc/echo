1849849aedd358aa9d131e300093e409d3f6e6c3
MinaAlmasi <mina.almasi00@gmail.com>
2023-11-23 13:29:07 +0100

    add system prompt (unfinished)

--- src/generate/prompts.py
+++ src/generate/prompts.py
@@ -2,7 +2,18 @@
 Functions for generating prompts
 '''
 from data import load_file
-def get_task_prompt(dataset, prompt_number): 
+def get_task_prompt(dataset:str, prompt_number:int): 
+    '''
+    Retrieve task prompt from specified dataset
+
+    Args
+        dataset: choose between ["dailymail_cnn", "stories", "mrpc", "dailydialog"]
+        prompt_number: number from 1 and upwards. Yields ValueError if prompt_number does not exist.
+
+    Returns
+        prompt: prompt for the particular dataset and prompt number. 
+    '''
+
     prompts = {
         # daily mail (summarization)
         "dailymail_cnn_1": "summarize the main points of this article: ",
@@ -33,16 +44,28 @@ def get_task_prompt(dataset, prompt_number):
         "dailydialog_4": "continue writing the next sentence in this: "
         }
 
+    valid_datasets = ["dailymail_cnn", "stories", "mrpc", "dailydialog"]
+
+    if dataset not in valid_datasets:
+        valid_datasets = ", ".join(valid_datasets)
+        raise ValueError(f"Invalid dataset '{dataset}'. Choose from {valid_datasets}")
+
+    # get prompt 
     key = f"{dataset}_{prompt_number}"
     prompt = prompts.get(key, None)
 
     if prompt is None:
-        raise ValueError(f"Invalid dataset '{dataset}' or prompt number '{prompt_number}'")
+        raise ValueError(f"Invalid prompt number '{prompt_number}' for dataset '{dataset}'")
 
     return prompt
 
-def get_system_prompt():
-    pass
+def get_system_prompt(chosen_model):
+    system_prompts = {
+            "beluga": "You are StableBeluga, an AI that follows instructions extremely well. Help as much as you can. Remember, be safe, and don't do anything illegal.\n\n",
+            "llama2_chat": "You are an AI, but you do not deviate from the task prompt and you do not small talk. Never begin your response with 'Sure, here is my response: ' or anything of the like. It is important that you finish without getting cut off."
+        }
+
+    # extract
 
 def add_task_prompt(df, dataset="stories", prompt_number=1):
     '''
@@ -55,9 +78,15 @@ def add_task_prompt(df, dataset="stories", prompt_number=1):
 
     return df 
 
+def add_system_prompt(df, dataset="stories", prompt_number=1):
+    '''
+    Add system prompt for beluga and llama2chat 
+    '''
+    pass
+
 def main():
     import pathlib
-    dataset = "stories"
+    dataset = "dailydialog"
     prompt_number = 2
     prompt = get_task_prompt(dataset, prompt_number)
     print(prompt)
