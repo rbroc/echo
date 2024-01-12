2a42c89510d0f57ac9b63f8fca3a20221f727064
MinaAlmasi <mina.almasi00@gmail.com>
2024-01-12 13:51:43 +0100

    rm wrong -

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ pip install -r requirements.txt
 
 # upgrade for quantized mdls (if quantized mdls annoy)
 #echo "[INFO]: Upgrading and installing for Quantized Mdls"
-#pip install --upgrade transformers -optimum 
+#pip install --upgrade transformers optimum 
 #pip install --upgrade auto-gptq
 
 # for text descriptives 
