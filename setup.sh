053f37f225cc420e064488d29bd15486f0d35fb7
MinaAlmasi <mina.almasi00@gmail.com>
2024-01-12 11:35:02 +0100

    add info msg

--- setup.sh
+++ setup.sh
@@ -2,6 +2,12 @@
 
 python3 -m venv env
 source ./env/bin/activate
+
+echo "[INFO]: Installing necessary reqs in env" 
 pip install -r requirements.txt
+
+# for text descriptives 
 python -m spacy download en_core_web_md
+
 deactivate
+echo "[INFO]: Done!" 
