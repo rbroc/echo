7308d10de98ed4997fd2e0948b3b8b8cbaae688b
MinaAlmasi <mina.almasi00@gmail.com>
2024-01-12 13:41:53 +0100

    update reqs for quantized mdls

--- setup.sh
+++ setup.sh
@@ -6,6 +6,11 @@ source ./env/bin/activate
 echo "[INFO]: Installing necessary reqs in env" 
 pip install -r requirements.txt
 
+# upgrade for quantized mdls (if quantized mdls annoy)
+#echo "[INFO]: Upgrading and installing for Quantized Mdls"
+#pip install --upgrade transformers -optimum 
+#pip install --upgrade auto-gptq
+
 # for text descriptives 
 python -m spacy download en_core_web_md
 
