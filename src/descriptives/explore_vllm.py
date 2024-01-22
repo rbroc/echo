12ae2daa734c2a6473c3ba7821252213c130ce31
MinaAlmasi <mina.almasi00@gmail.com>
2024-01-22 14:11:44 +0100

    investigate vllm

--- src/descriptives/explore_vllm.py
+++ src/descriptives/explore_vllm.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 import sys 
 sys.path.append(str(pathlib.Path(__file__).parents[2]))
 from src.utils.pca import get_descriptive_metrics, run_PCA
-from src.utils.distance import compute_distances, jitterplots
+from src.utils.distance import compute_distances
 
-def explore_data(model="beluga7b", file="stories_prompt_1.ndjson", vllm=False, root_data_path=pathlib.Path(__file__).parents[2] / "datasets"):
+def load_data(model="beluga7b", file="stories_prompt_1.ndjson", vllm=False, root_data_path=pathlib.Path(__file__).parents[2] / "datasets"):
     if vllm:
         datapath = root_data_path / "ai_vllm_datasets" / "ALL_DATA"
     else: 
@@ -19,31 +19,21 @@ def explore_data(model="beluga7b", file="stories_prompt_1.ndjson", vllm=False, r
     filepath = datapath / model / file 
     df = pd.read_json(filepath, lines=True)
 
-    # get mean length 
-    mean_length =  df[f"{model}_completions"].apply(len).mean()
-    print(f"Mean Length: {mean_length}")
-
     return df 
 
-def plot_string_length_distribution_from_df(df, model="beluga7b", col="completions", framework_col='framework', bins=30, figsize=(10, 6), title='String Lengths by Framework', save_path=None):
-    full_column_name = f"{model}_{col}"
-    if full_column_name not in df.columns or framework_col not in df.columns:
-        raise ValueError(f"Required columns not found in DataFrame.")
-
-    df['string_length'] = df[full_column_name].astype(str).apply(len)
-
+def plot_distribution(df, col="doc_length", hue_col='model', bins=30, figsize=(10, 6), title='Doc Lengths by Framework', save_path=None):
     # Set the style for a prettier plot
     sns.set(style="whitegrid")
 
     # Plotting the histogram
     plt.figure(figsize=figsize)
-    sns.histplot(data=df, x='string_length', hue=framework_col, kde=False, bins=bins, palette='viridis')
+    sns.histplot(data=df, x='doc_length', hue=hue_col, kde=False, bins=bins, palette='viridis')
     plt.title(title)
-    plt.xlabel('Length of Strings')
+    plt.xlabel('Length of Documents (SpaCy doc length)')
     plt.ylabel('Frequency')
 
     if save_path:
-        plt.savefig(save_path, bbox_inches='tight')
+        plt.savefig(save_path, bbox_inches='tight', dpi=300)
 
 def main(): 
     path = pathlib.Path(__file__)
@@ -51,15 +41,15 @@ def main():
     model = "beluga7b"
 
     # plot 
-    vllm = explore_data(vllm=True)
-    hf = explore_data(vllm=False)
+    vllm = load_data(vllm=True)
+    hf = load_data(vllm=False)
 
-    vllm = vllm[:900]
-    hf = hf[:900]
+    vllm = vllm[:1000]
+    hf = hf[:1000]
 
     # add framework 
-    vllm["framework"] = "vllm"
-    hf["framework"] = "hf"
+    vllm["model"] = "vllm"
+    hf["model"] = "hf"
 
     # drop cols for both
     for df in [vllm, hf]:
@@ -68,14 +58,10 @@ def main():
     # concatenate 
     df = pd.concat([vllm, hf], axis=0, ignore_index=True)
 
-    # plot 
-    plot_string_length_distribution_from_df(df, save_path=path.parents[2] / "results" / "string_length_distribution.png")
-
     ## PCA ## 
     spacy.util.fix_random_seed(129)
 
     # rename framework col to model
-    df.rename(columns={"framework": "model"}, inplace=True)
     df.rename(columns={"beluga7b_completions": "completions"}, inplace=True)
     df["dataset"] = "stories"
     df["prompt_number"] = 1
@@ -93,10 +79,27 @@ def main():
     print(final_df)
     print(pca.explained_variance_ratio_)
 
-    print("[INFO:] COMPUTING & PLOTTING DISTABCES ...")
-    distance_df = compute_distances(final_df, models=["vllm"], baseline="hf")
-    jitterplots(distance_df, ["stories"], save_path= path.parents[0])
+    # print mean doc length per framework
+    print(final_df.groupby("model")["doc_length"].mean())
 
+    # plot 
+    plot_distribution(final_df, hue_col="model", save_path=path.parents[0] / "doc_length_distribution.png")
+
+    # remove rows with doc length below 112 (min length) 
+    # since vllm does not have a min tokens param, it is to avoid those where it is bugged to make a more fair comparison with hf framework implementation
+    final_df = final_df[final_df["doc_length"] > 112]
+
+    # make sure that there is 2 of each id (one for each model). If not, drop the row
+    final_df = final_df.groupby("id").filter(lambda x: len(x) == 2)
+
+    print(len(final_df))
+
+    print("[INFO:] COMPUTING & PLOTTING DISTANCES ...")
+    distance_df = compute_distances(final_df, models=["vllm"], baseline="hf")
+    g = sns.catplot(data=distance_df, x="prompt_number", y="distance", kind="strip", jitter=0.3)
+    g.set(xlabel=None, xticklabels=[])
+    plt.title("Distance between VLLM and HF \n Stories dataset, Completions below 112 doc length removed")
+    g.savefig(path.parents[0] / "vllm_hf_distance_stories.png", dpi=600)
 
 if __name__ == "__main__":
     main()
