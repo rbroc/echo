7c6668a7c1d26c3a359f7d184aaaf52371eef8e7
MinaAlmasi <mina.almasi00@gmail.com>
2024-01-23 13:27:00 +0100

    add pipeline with vllm and hf functionality

--- src/generate/run_pipeline.py
+++ src/generate/run_pipeline.py
@@ -1,13 +1,27 @@
 '''
-Pipeline to generate AI completions with various models using Hugging Face's pipeline() function. 
+Pipeline to generate AI completions with various models. 
+
+Run the script in the terminal by typing:
+    python src/generate/run_pipeline.py -d {DATASET} -mdl {MODEL_NAME} -prompt_n {PROMPT_NUMBER} -subset {DATA_SUBSET} 
+
+Args
+    -d -> dataset, choose between "stories", "dailymail_cnn", "mrpc" and "dailydialog"
+    -mdl -> model name, choose between "beluga7b", "beluga70b", "beluga70bQ", "llama2_chat13b", "llama2_chat70bQ", "mistral7b", "mistral8x7b"
+    -prompt_n -> task prompt defined in prompts.py for each dataset. Choose between 1 to 6.
+    -subset -> how many rows to include in the generations. Defaults to None (i.e., all rows). Otherwise takes the n first rows. Mainly for testing. 
+
+The additional flag -hf will trigger an Hugging Face implmentation of the model as opposed to vLLM: 
+    python src/generate/run_pipeline.py -hf -batch {BATCH_SIZE}
+
+NOTE: the -batch argument which allows processing in parallel with HF (vLLM does this automatically). 
 '''
 import argparse
 import pathlib
 from transformers import set_seed
 
 # custom 
-from models import FullModel, QuantizedModel
-from generation import load_json_data, extract_min_max_tokens, login_hf_token, hf_generate
+from models import FullModel, QuantizedModel, vLLM_Model
+from generation import load_json_data, extract_min_max_tokens, login_hf_token, hf_generate, vllm_generate
 from prompts import add_prompts_to_df
 
 def input_parse():
@@ -18,22 +32,23 @@ def input_parse():
     parser.add_argument("-mdl", "--model_name", help = "Choose between models ...", type = str, default = "beluga7b")
     parser.add_argument("-prompt_n", "--prompt_number", help = "choose which prompt to use", type = int, default = 1)
     parser.add_argument("-subset", "--data_subset", help = "how many rows you want to include. Useful for testing. Defaults to None.", type = int, default=None)
-    parser.add_argument("-batch", "--batch_size", help = "Batching of dataset. Mainly for processing in parallel for GPU. Defaults to no batching (batch size of 1). ", type = int, default=1)
+    parser.add_argument("-batch", "--batch_size", help = "Batching of dataset. Only for HF for processing in parallel for GPU. Defaults to no batching (batch size of 1). ", type = int, default=1)
+    parser.add_argument("-hf", "--use_hf_pipeline", help="Use HF pipeline if set, otherwise use vLLM", action='store_true')
 
     # save arguments to be parsed from the CLI
     args = parser.parse_args()
 
     return args
 
-def main(): 
-    # seed, only necessary if prob_sampling params such as temperature is defined
+def main():
+    # only relevant for probability sampling / decoding 
     set_seed(129)
 
-    # init args, define path 
+    # args, pathing 
     args = input_parse()
     path = pathlib.Path(__file__)
 
-    ## LOAD DATA ##
+    # load data
     dataset  = args.dataset
     datapath = path.parents[2] / "datasets" / "human_datasets" / dataset
     datafile = datapath / "data.ndjson"
@@ -41,24 +56,47 @@ def main():
     df = load_json_data(datafilepath=datafile, n_subset=args.data_subset)
     min_len, max_tokens = extract_min_max_tokens(dataset)
 
-    # subset data for prompting. saves to "datasets_ai" / "model_name". If data is not subsetted, will save data to full_data / "model_name"
-    if args.data_subset is not None: 
-        outpath = path.parents[2] / "datasets" / "ai_datasets" / f"{args.model_name}" 
-    else:
-        outpath = path.parents[2] / "datasets" / "ai_datasets" / "ALL_DATA" / f"{args.model_name}" 
-
-    outpath.mkdir(parents=True, exist_ok=True)
-
-    ## LOAD MDL ##
-    print(f"[INFO:] Instantiating model ...")
+    # chose model 
     chosen_model_name = args.model_name
     cache_models_path =  path.parents[3] / "models"
 
     # load token (for llama2)
     if "llama2_chat" in chosen_model_name: 
         login_hf_token()
+
+    # define sampling params
+    params = {"temperature":1, "top_k":50, "top_p":1, "repetition_penalty":1, "length_penalty":1}
+
+    # run pipeline
+    if args.use_hf_pipeline:
+        hf_pipeline(args, df, min_len=min_len, max_tokens=max_tokens, path, cache_models_path, chosen_model_name, sample_params=params)
+    else: 
+        vllm_pipeline(args, df, max_tokens, path, cache_models_path, chosen_model_name, sample_params=params)
     
+
+def hf_pipeline(args, df, min_len, max_tokens, path, cache_models_path=None, chosen_model_name, sample_params:dict=None):    
+    '''
+    Generation steps specific to a model implementation in HF
+
+    Args
+        args: CLI arguments
+        df: dataframe with data to generate completions for
+        min_len: minimum number of tokens to generate
+        max_tokens: maximum number of tokens to generate
+        path: path to this script
+        cache_models_path: path to cache models (if None, will redownload models)
+        chosen_model_name: name of the model to use
+        sample_params: dictionary of sampling params to use
+    '''
+    if args.data_subset is None:
+        outpath = path.parents[2] / "ai_datasets" / "HF" / "FULL_DATA" / f"{args.model_name}"
+    else:
+        outpath = path.parents[2] / "ai_datasets" / "HF" / "SUBSET_DATA" / f"{args.model_name}"
+
+    outpath.mkdir(parents=True, exist_ok=True)
+
     # init model object -> full or quantized model depending on mdl name (mdl will first be loaded in completions_generator). 
+    print(f"[INFO:] Instantiating model ...")
     if "Q" not in chosen_model_name: 
         model_obj = FullModel(chosen_model_name)
     else: 
@@ -67,25 +105,74 @@ def main():
     # format prompts depending on model # 
     prompt_df = add_prompts_to_df(model_obj, df, dataset=dataset, prompt_number=args.prompt_number) 
 
-    ## INIT GEN ## 
+    # set prob_sampling 
+    all_params=None
+
+    if sample_params: 
+        hf_params = {"do_sample":True}
+        all_params = {**hf_params, **sample_params}
+        print(f"Decoding params: {all_params}")
+
     print(f"[INFO:] Generating completions with {model_obj.full_model_name} ...")
-
-    # generate
-    prob_sampling = {"do_sample":True, "temperature":1}
-
     df_completions = hf_generate(
-                                                        hf_model=model_obj,
-                                                        df=prompt_df, 
-                                                        prompt_col=f"prompt_{args.prompt_number}", 
-                                                        min_len=min_len,
-                                                        max_tokens=max_tokens, 
-                                                        batch_size=args.batch_size, 
-                                                        sample_params = prob_sampling, # can be set to NONE to do no sampling
-                                                        outfilepath=outpath / f"{dataset}_prompt_{args.prompt_number}.ndjson",
-                                                        cache_dir=cache_models_path
-                                                        )
+        hf_model=model_obj,
+        df=prompt_df, 
+        prompt_col=f"prompt_{args.prompt_number}", 
+        min_len=min_len,
+        max_tokens=max_tokens, 
+        batch_size=args.batch_size, 
+        sample_params = all_params,
+        outfilepath=outpath / f"{dataset}_prompt_{args.prompt_number}.ndjson",
+        cache_dir=cache_models_path
+    )
+    print("[INFO:] HF Pipeline DONE!")
 
-    print("[INFO:] DONE!")
+def vllm_pipeline(args, df, max_tokens, path, cache_models_path=None, chosen_model_name, sample_params:dict=None):
+    '''
+    Generation steps specific to a model implementation with vLLM (https://github.com/vllm-project/vllm)
+
+    Args
+        args: CLI arguments
+        df: dataframe with data to generate completions for
+        max_tokens: maximum number of tokens to generate
+        path: path to this script
+        cache_models_path: path to cache models (if None, will redownload models)
+        chosen_model_name: name of the model to use
+        sample_params: dictionary of sampling params to use. 
+    '''
+    if args.data_subset is None:
+        outpath = path.parents[2] / "ai_datasets" / "vLLM" / "FULL_DATA" / f"{args.model_name}"
+    else:
+        outpath = path.parents[2] / "ai_datasets" / "vLLM" / "SUBSET_DATA" / f"{args.model_name}"
+
+    outpath.mkdir(parents=True, exist_ok=True)
+
+    # load LLM
+    model_obj = vLLM_Model(chosen_model_name)
+
+    # format prompts depending on the model 
+    prompt_df = add_prompts_to_df(model_obj, df, dataset=dataset, prompt_number=args.prompt_number) 
+
+    # setup 
+    all_params_obj=None
+
+    if sample_params: 
+        vllm_params = {"presence_penalty":0, "frequency_penalty":0} # set to 0 as they do not exist in HF framework
+        all_params = {**vllm_params, **sample_params}
+        print(f"Decoding params: {all_params}")
+        all_params_obj = SamplingParams(**all_params)
+
+    print(f"[INFO:] Generating completions with {model_obj.full_model_name} ...")
+    df_completions = vllm_generate(
+        vllm_model=model_obj, 
+        df=prompt_df, 
+        prompt_col=f"prompt_{args.prompt_number}", 
+        max_tokens=max_tokens, 
+        sample_params = all_params_obj,
+        outfilepath=outpath / f"{dataset}_prompt_{args.prompt_number}.ndjson",
+        cache_dir=cache_models_path
+    )
+    print("[INFO:] vLLM Pipeline DONE!")
 
 if __name__ == "__main__":
     main()
