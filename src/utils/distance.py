2703e23216d339ce3d11e1287e637612608dd910
MinaAlmasi <mina.almasi00@gmail.com>
2024-01-30 14:16:48 +0100

    add option to save distances

--- src/utils/distance.py
+++ src/utils/distance.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 
-def compute_distances(df: pd.DataFrame, models: list = ["beluga7b", "llama2_chat13b"], cols: list = ["PC1", "PC2", "PC3", "PC4"], baseline: str = "human", include_baseline_completions:bool=False):
+def compute_distances(df: pd.DataFrame, models: list = ["beluga7b", "llama2_chat13b"], cols: list = ["PC1", "PC2", "PC3", "PC4"], baseline: str = "human", include_baseline_completions:bool=False, save_path=None):
     '''
     Extract euclidean distances between human and model completions in n-dimensions from a list of features (cols) 
 
@@ -66,7 +66,16 @@ def compute_distances(df: pd.DataFrame, models: list = ["beluga7b", "llama2_chat
 
     result_df = pd.DataFrame(result_rows)
 
-    return result_df
+    # sort by id and model
+    sorted_df = result_df.copy().sort_values(["id", "model"])
+
+    if save_path:
+        save_path.mkdir(parents=True, exist_ok=True)
+        save_file = save_path / "distances_PC_cols.csv"
+        
+        sorted_df.to_csv(save_file, index=False)
+
+    return sorted_df
 
 # plotting # 
 def jitterplots(data:pd.DataFrame, datasets:list, save_path:pathlib.Path):
