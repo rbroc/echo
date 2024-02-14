84339037b6d6091334ba96a1c5007d0a465ee2be
MinaAlmasi <mina.almasi00@gmail.com>
2024-02-14 20:06:22 +0100

    experiment w. prompts

--- src/generate/prompts.py
+++ src/generate/prompts.py
@@ -52,6 +52,7 @@ def get_task_prompt(dataset:str, prompt_number:int):
         "dailymail_cnn_21": "summarize this in a few sentences: ",
         "mrpc_21": "paraphrase this: ",
         "stories_21": "write a story based on this: ", 
+        "stories_22": "continue the story: ", 
         "dailydialog_21": "continue the conversation between A and B by writing a single response to the latest speaker. write only a concise response and nothing else: ", 
     }
 
