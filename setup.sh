0afb6f841c573d42f7b786595b5a95d336f8806c
MinaAlmasi <mina.almasi00@gmail.com>
2024-02-23 10:40:49 +0100

    update vvlm + spacy, add textdescriptives as install after reqs, add spacy to reqs instead of setup

--- setup.sh
+++ setup.sh
@@ -5,16 +5,12 @@ source ./env/bin/activate
 
 echo "[INFO]: Installing necessary reqs in env" 
 pip install -r requirements.txt
+pip install textdescriptives==2.7.3
 
 # upgrade for quantized mdls (if quantized mdls annoy)
 #echo "[INFO]: Upgrading and installing for Quantized Mdls"
 #pip install --upgrade transformers optimum 
 #pip install --upgrade auto-gptq
-
-# for text descriptives 
-python -m spacy download en_core_web_sm
-python -m spacy download en_core_web_md
-python -m spacy download en_core_web_lg
 
 deactivate
 echo "[INFO]: Done!" 
