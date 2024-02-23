26000330d03093e5e81af05cd32cf3e6fb6cdc0f
MinaAlmasi <mina.almasi00@gmail.com>
2024-02-23 14:32:50 +0100

    fix plot (still needs some tinkering)

--- src/inspect/check_length.py
+++ src/inspect/check_length.py
@@ -6,7 +6,7 @@ import sys
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
+from matplotlib.colors import ListedColormap
 import argparse
 import spacy 
 sys.path.append(str(pathlib.Path(__file__).parents[2]))
@@ -18,30 +18,41 @@ def input_parse():
     args = parser.parse_args()
     return args
 
-def plot_distribution(df, col="doc_length", hue_col='model', bins=30, figsize=(10, 6), title='Doc Lengths by Framework', save_path=None):
-    sns.set(style="whitegrid")
-
+def plot_lengths_per_dataset(df, col="doc_length", hue_col='model', bins=30, figsize=(12, 6), title='Doc Lengths by Framework', save_path=None):
     unique_datasets = df['dataset'].unique()
     num_datasets = len(unique_datasets)
     fig, axes = plt.subplots(2, 2, figsize=figsize)
     axes = axes.flatten()
 
+    colormap = sns.color_palette("viridis").as_hex()
+
     for i, dataset in enumerate(unique_datasets):
         # filter dataframe for current dataset
         dataset_df = df[df['dataset'] == dataset]
 
-        # plot
-        sns.histplot(data=dataset_df, x=col, hue=hue_col, kde=False, bins=bins, palette='viridis', ax=axes[i])
+        # plot each hue (e.g., each model)
+        ax = axes[i]
+        for j, (hue_val, hue_df) in enumerate(dataset_df.groupby(hue_col, sort=False)):
+            if hue_val == "human":
+                ax.hist(x=col, bins=bins, data=hue_df, alpha=0.6, label=hue_val, color='red', edgecolor="white")
+            else: 
+                ax.hist(x=col, bins=bins, data=hue_df, alpha=0.6, label=hue_val, color=colormap[j], edgecolor="white")
 
-        # adjust
-        axes[i].xaxis.set_major_locator(ticker.MultipleLocator(100))
-        axes[i].set_title(f'{dataset}')
-    
-    fig.text(0.5, 0.04, 'Length of Documents (SpaCy doc length)', ha='center')
-    fig.text(0.04, 0.5, 'Frequency', va='center', rotation='vertical')
+        # add contents
+        ax.set_title(f'{dataset.upper()}')
+        ax.set_xlabel('')
+        ax.set_ylabel('')
 
-    fig.suptitle(title)
-    
+    fig.suptitle(title, fontsize=16)
+    fig.subplots_adjust(bottom=0.1)
+    fig.supxlabel("Length of Documents (SpaCy doc length)")
+    fig.supylabel("Frequency")
+    plt.tight_layout(pad=1.5)
+
+    # extract legend from last plot and place it in the in fig.legend underneath suptitle
+    handles, labels = ax.get_legend_handles_labels()
+    fig.legend(handles, labels, loc='lower center', ncol=num_datasets, bbox_to_anchor=(0.5, -0.06))
+
     if save_path:
         plt.savefig(save_path, bbox_inches='tight', dpi=300)
 
@@ -68,12 +79,13 @@ def main():
             df.at[i, "doc_length"] = len(doc)
 
     # plot
-    plot_distribution(
+    plot_lengths_per_dataset(
                     df, 
                     col="doc_length", 
                     hue_col='model', 
-                    bins=50, 
-                    figsize=(10, 6), 
+                    bins=30, 
+                    figsize=(12, 8), 
+                    title = f"Doc Lengths by Framework (Temperature: {temperature})",
                     save_path=path.parents[0] / f"temp{temperature}_doc_lengths.png"
                     )                 
 
