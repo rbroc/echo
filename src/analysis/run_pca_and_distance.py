429e70681eda824414fbe1ff9f628a2e970a8f7c
MinaAlmasi <mina.almasi00@gmail.com>
2024-02-27 17:55:14 +0100

    update paths

--- src/analysis/run_pca_and_distance.py
+++ src/analysis/run_pca_and_distance.py
@@ -3,17 +3,17 @@ Script to investigate generated data
 '''
 import pathlib
 import sys
-sys.path.append(str(pathlib.Path(__file__).parents[3]))
+sys.path.append(str(pathlib.Path(__file__).parents[2]))
 from src.utils.process_generations import preprocess_datasets
 from src.utils.pca import get_descriptive_metrics, run_PCA, save_PCA_results, get_loadings, plot_loadings
 from src.utils.distance import compute_distances, jitterplots, interactive_jitterplot
 
 def main(): 
     path = pathlib.Path(__file__)
-    ai_dir = path.parents[3] / "datasets" / "ai_datasets" / "vLLM" / "FULL_DATA"
-    human_dir = path.parents[3] / "datasets" / "human_datasets"
+    ai_dir = path.parents[2] / "datasets" / "ai_datasets" / "vLLM" / "FULL_DATA"
+    human_dir = path.parents[2] / "datasets" / "human_datasets"
 
-    results_path = path.parents[3] / "results" / "analysis"
+    results_path = path.parents[2] / "results" / "analysis"
     pca_path = results_path / "PCA"
     distance_path = results_path / "distance"
 
