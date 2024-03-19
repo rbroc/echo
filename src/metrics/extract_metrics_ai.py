163f0f23f4cd084dd47820159973b4b6d0b69e79
MinaAlmasi <mina.almasi00@gmail.com>
2024-03-19 15:14:41 +0100

    add dummy fn to test pipeline

--- src/metrics/extract_metrics_ai.py
+++ src/metrics/extract_metrics_ai.py
@@ -3,6 +3,7 @@ import spacy
 import textdescriptives as td
 from argparse import ArgumentParser
 import pathlib 
+import numpy as np
 
 import sys
 sys.path.append(str(pathlib.Path(__file__).parents[2]))
@@ -17,6 +18,25 @@ def input_parse():
 
     return args
 
+def get_metrics_dummy(df, text_column:str, spacy_mdl:str="en_core_web_md"):
+    '''
+    dummy function to test pipeline
+    '''
+    n_features = 2
+
+    # create new df 
+    metrics = pd.DataFrame()
+    
+    # add n_features worth of dummy features
+    print(f"Extracting {n_features} dummy features not using {spacy_mdl} and not doing it on text column {text_column} ...")
+    for i in range(n_features):
+        metrics[f"dummy_feature_{i}"] = np.random.randint(0, 1000, size=len(df))
+
+    # add to existing df
+    metrics_df = pd.concat([df, metrics], axis=1)
+
+    return metrics_df
+
 def main(): 
     args = input_parse()
 
@@ -24,14 +44,15 @@ def main():
     path = pathlib.Path(__file__)
     ai_dir = path.parents[2] / "datasets" / "ai_datasets" / "vLLM" / "FULL_DATA"
 
-    results_path = path.parents[2] / "results" / "metrics" / "ai_metrics"
-    results_path.mkdir(parents=True, exist_ok=True)
+    metrics_path = path.parents[2] / "metrics" / "ai_metrics"
+    metrics_path.mkdir(parents=True, exist_ok=True)
 
     # models
-    models = ["beluga7b", "llama2_chat13b", "mistral7b"]
+    models = ["beluga7b", "llama2_chat13b", "mistral7b", "llama2_chat7b"]
 
     # load paths
-    ai_paths = get_ai_paths(ai_dir=ai_dir, models=models, dataset=args.dataset, temp=1, prompt_numbers=[21, 22])
+    temp = 1 
+    ai_paths = get_ai_paths(ai_dir=ai_dir, models=models, dataset=args.dataset, temp=temp, prompt_numbers=[21])
 
     # load df
     ai_dfs = [pd.read_json(p, lines=True) for p in ai_paths]
@@ -43,10 +64,10 @@ def main():
     df = pd.concat(ai_dfs_formatted, ignore_index=True, axis=0)
 
     # get metrics
-    metrics = get_all_metrics(df, "completions", "en_core_web_md")
+    metrics = get_metrics_dummy(df, "completions", "en_core_web_md")
     
     # save
-    metrics.to_csv(results_path / f"{args.dataset}_completions.csv")
+    metrics.to_csv(metrics_path / f"dummy_{args.dataset}_completions_temp{temp}.csv")
 
 
 if __name__ == "__main__":
