e90fdf13bc431152756e515fcec883fe2079aad7
MinaAlmasi <mina.almasi00@gmail.com>
2024-03-22 10:34:24 +0100

    mv ai pipeline from main to own fn

--- src/metrics/extract_metrics_ai.py
+++ src/metrics/extract_metrics_ai.py
@@ -22,7 +22,7 @@ def get_metrics_dummy(df, text_column:str, spacy_mdl:str="en_core_web_md"):
     '''
     dummy function to test pipeline
     '''
-    n_features = 2
+    n_features = 10
 
     # create new df 
     metrics = pd.DataFrame()
@@ -37,6 +37,43 @@ def get_metrics_dummy(df, text_column:str, spacy_mdl:str="en_core_web_md"):
 
     return metrics_df
 
+def get_ai_metrics(ai_dir, models=["beluga7b", "llama2_chat13b", "mistral7b", "llama2_chat7b"], dataset:str="mrpc", temp:int|float=1, save_path=None):
+    '''
+    Extract metrics for AI completions
+    '''
+    # load path, only for prompt_numbers 21 (as they are the 2.0 prompts that we settled on, but function is capable of loading whatever you want!)
+    ai_paths = get_ai_paths(ai_dir=ai_dir, models=models, dataset=dataset, temp=temp, prompt_numbers=[21]) 
+
+    # load df 
+    ai_dfs = [pd.read_json(ai_path, lines=True) for ai_path in ai_paths]
+
+    # format dfs using custom fn
+    ai_dfs_formatted = format_ai_data(ai_dfs)
+
+    # concat
+    df = pd.concat(ai_dfs_formatted, ignore_index=True, axis=0)
+
+    # drop doc length (as metrics adds it)
+    df = df.drop(columns=["doc_length"])
+
+    # extract metrics
+    metrics_df = get_metrics_dummy(df, "completions", "en_core_web_md")
+
+    # drop cols 
+    metrics_df = metrics_df.drop(columns=["completions", "prompt"])
+
+    # mv model col to front if present in df 
+    if "model" in metrics_df.columns: 
+        metrics_df.insert(loc=1, column='model', value=metrics_df.pop('model')) # insert mdl col on 2nd position in df  
+
+    if save_path:
+        metrics_df.to_csv(save_path)
+
+    return metrics_df
+
+def get_human_metrics(human_dir, dataset:str, save_path=None):
+    pass
+
 def main(): 
     args = input_parse()
 
@@ -47,27 +84,16 @@ def main():
     metrics_path = path.parents[2] / "metrics" / "ai_metrics"
     metrics_path.mkdir(parents=True, exist_ok=True)
 
-    # models
-    models = ["beluga7b", "llama2_chat13b", "mistral7b", "llama2_chat7b"]
 
-    # load paths
-    temp = 1 
-    ai_paths = get_ai_paths(ai_dir=ai_dir, models=models, dataset=args.dataset, temp=temp, prompt_numbers=[21])
+    # define args
+    temp = 1
+    save_path = metrics_path / f"dummy_{args.dataset}_completions_temp{temp}.csv"
 
-    # load df
-    ai_dfs = [pd.read_json(p, lines=True) for p in ai_paths]
+    metrics_df = get_ai_metrics(ai_dir=ai_dir, 
+                                models=["beluga7b", "llama2_chat13b", "mistral7b", "llama2_chat7b"], 
+                                dataset=args.dataset, temp=temp, save_path=save_path
+                                )
 
-    # combine
-    ai_dfs_formatted = format_ai_data(ai_dfs)
-
-    # process
-    df = pd.concat(ai_dfs_formatted, ignore_index=True, axis=0)
-
-    # get metrics
-    metrics = get_metrics_dummy(df, "completions", "en_core_web_md")
-    
-    # save
-    metrics.to_csv(metrics_path / f"dummy_{args.dataset}_completions_temp{temp}.csv")
 
 
 if __name__ == "__main__":
