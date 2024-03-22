11b1db65856d6f37fdbefd1593d2ba4114034f1c
MinaAlmasi <mina.almasi00@gmail.com>
2024-03-22 11:19:43 +0100

    refactor fns, add a metrics fn using nlp.pipe() that allows multiprocesing

--- src/utils/get_metrics.py
+++ src/utils/get_metrics.py
@@ -9,23 +9,58 @@ def get_descriptive_metrics(df:pd.DataFrame, text_column:str, spacy_mdl:str="en_
     '''
     Extract low level descriptive features doc_length, n_tokens, n_characters and n_sentences 
     '''
-    textcol = df[text_column]
+    # get text
+    text = df[text_column]
 
-    metrics = td.extract_metrics(text=textcol, spacy_model=spacy_mdl, metrics=["descriptive_stats", "quality"])
-    subset_metrics = metrics[["doc_length", "n_tokens", "n_characters", "n_sentences"]]
+    # extract metrics, select only relevant cols 
+    metrics_df = td.extract_metrics(text=text, spacy_model=spacy_mdl, metrics=["descriptive_stats", "quality"])
+    subset_metrics_df = metrics[["doc_length", "n_tokens", "n_characters", "n_sentences"]]
 
-    metrics_df = pd.concat([df, subset_metrics], axis=1)
+    # combine with df 
+    final_df = pd.concat([df, subset_metrics_df], axis=1)
     
-    return metrics_df 
+    return final_df 
 
 
 def get_all_metrics(df:pd.DataFrame, text_column:str, spacy_mdl:str="en_core_web_md"):
     '''
     Extract all metrics using textdescriptives
     '''
-    textcol = df[text_column]
+    # get text
+    text = df[text_column]
 
-    metrics = td.extract_metrics(text=textcol, spacy_model=spacy_mdl)
-    metrics_df = pd.concat([df, metrics], axis=1)
+    # extract text
+    metrics_df = td.extract_metrics(text=text, spacy_model=spacy_mdl)
+
+    # combine with df 
+    final_df = pd.concat([df, metrics_df], axis=1)
     
-    return metrics_df
+    return final_df
+
+
+def get_all_metrics_pipe(df:pd.DataFrame, text_column:str, batch_size:int=1, n_process:int=1, spacy_mdl:str="en_core_web_md"):
+    '''
+    Extract all metrics using textdescriptives using nlp.pipe(). 
+
+    Same functionality as td.extract_metrics() but allows for multiprocessing. 
+    '''
+    # load nlp, add td to model
+    nlp = spacy.load(spacy_mdl)
+    nlp.add_pipe("textdescriptives/all")
+
+    # get txt
+    text = df[text_column]
+
+    # pass txt to pipeline
+    docs = nlp.pipe(text, batch_size=batch_size, n_process=n_process)
+
+    # get metrics as df 
+    metrics_df = td.extract_df(docs, include_text=False)
+
+    # sort columns alphabetically
+    metrics_df = metrics_df.reindex(sorted(metrics_df.columns), axis=1)
+
+    # concat 
+    final_df = pd.concat([df, metrics_df], axis=1)
+
+    return final_df
