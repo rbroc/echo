ac79b1f632ffb12b1476c34a64d2c949990e6f9f
MinaAlmasi <mina.almasi00@gmail.com>
2024-04-07 18:47:30 +0200

    feat: init empty classify.py

---
+++ src/classify/classify.py
@@ -0,0 +1,16 @@
+'''
+Construct classifiers
+'''
+from xgboost import XGBClassifier
+
+def clf_fit(classifier, X_train, y_train): 
+    pass
+
+def clf_evaluate(classifier, X, y):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
