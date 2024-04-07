b58be5397edb186d1732d7e676e5ff878d2950bf
MinaAlmasi <mina.almasi00@gmail.com>
2024-04-07 13:14:19 +0200

    fix: update datapath

--- src/metrics/explore_metrics.py
+++ src/metrics/explore_metrics.py
@@ -63,7 +63,7 @@ def main():
 
     # define paths 
     path = pathlib.Path(__file__)
-    datapath = path.parents[2] / "results" / "metrics" / "human_metrics" 
+    datapath = path.parents[2] / "metrics" / "human_metrics" 
 
     min_lengths = []
     max_lengths = []
