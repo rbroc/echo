18ece3a63fb28ca253f866b083fb64b47b214ec9
MinaAlmasi <mina.almasi00@gmail.com>
2024-04-09 10:12:11 +0200

    feat: add input parse to classify

--- src/classify/classify.py
+++ src/classify/classify.py
@@ -2,6 +2,8 @@
 Construct classifiers
 '''
 import pathlib
+import numpy as np # rm this at some point
+import argparse
 from xgboost import XGBClassifier
 from sklearn import metrics
 from prepare_data import load_metrics, create_split
@@ -24,26 +26,44 @@ def clf_evaluate(classifier, X, y):
 
     return clf_report
 
+def input_parse():
+    parser = argparse.ArgumentParser()
+
+    # add dataset as arg 
+    parser.add_argument("-d", "--dataset", default="dailymail_cnn", help="Choose between 'stories', 'dailymail_cnn', 'mrpc', 'dailydialog'", type=str)
+
+    args = parser.parse_args()
+
+    return args
+
 def main():
+    args = input_parse()
+
     # initialize classifier
-    clf = XGBClassifier(enable_categorical=True, use_label_encoder=False)
+    clf = XGBClassifier(enable_categorical=True, use_label_encoder=False, random_state=129)
 
     # load data, create splits
     path = pathlib.Path(__file__)
     datapath = path.parents[2] / "metrics"
 
-    final_df = load_metrics(datapath, dataset="stories", temp=1)
+    df = load_metrics(datapath, dataset=args.dataset, temp=1)
 
-    splits = create_split(final_df, random_state=129, val_test_size=0.15, outcome_col="is_human", verbose=False, feature_cols=["passed_quality_check"])
+    # xgboost bugs with the dataset with these two cols "Check failed: valid: Input data contains `inf` or a value too large, while `missing` is not set to `inf`", so temporarily dropped 
+    # may be because there are some very large values in the bottom (could be a solution to scale instead)
+    if args.dataset == "dailymail_cnn":
+        df = df.drop(columns=["per_word_perplexity", "perplexity"])
+
+    # make train, val, test splits
+    splits = create_split(df, random_state=129, val_test_size=0.15, outcome_col="is_human", verbose=False)
 
     # fit classifier
     clf = clf_fit(clf, splits["X_train"], splits["y_train"])
 
-    print(splits["X_train"].columns)
-
     # evaluate classifier on val set
     clf_report = clf_evaluate(clf, X=splits["X_val"], y=splits["y_val"])
 
+    print(splits["y_train"].value_counts())
+    print(splits["y_val"].value_counts())
     print(splits["y_test"].value_counts())
 
     print(clf_report)
