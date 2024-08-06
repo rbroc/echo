df288c42100bcc2ff639fdf7203958f116d242c9
MinaAlmasi <mina.almasi00@gmail.com>
2024-08-06 15:50:35 +0200

    fix: run pca with standardised columns to drop (the same cols across datasets)

--- src/classify/run_pca.py
+++ src/classify/run_pca.py
@@ -7,49 +7,7 @@ import matplotlib.pyplot as plt
 import sys
 sys.path.append(str(pathlib.Path(__file__).parents[2]))
 from src.utils.pca import run_PCA, save_PCA_results
-from prepare_metrics import load_metrics, filter_metrics
-
-# functions currently not in use!! For plotting loadings 
-def plot_cumulative_variance(pca, title, save_dir=None, file_name=None): 
-    '''
-    plot cumulative explained variance for pca 
-
-    code by https://jakevdp.github.io/PythonDataScienceHandbook/05.09-principal-component-analysis.html 
-    '''
-    # close previous plot
-    plt.close()
-
-    sns.set_theme(rc={'figure.figsize':(10, 10)}) # see https://jakevdp.github.io/PythonDataScienceHandbook/05.09-principal-component-analysis.html 
-    plt.plot(np.cumsum(pca.explained_variance_ratio_))
-    plt.xlabel('number of components')
-    plt.ylabel('cumulative explained variance')
-    plt.title(title)
-
-    if save_dir and file_name:
-        save_dir.mkdir(parents=True, exist_ok=True)
-        plt.savefig(save_dir / file_name)
-
-def create_loadings(pca, feature_names):
-    # see stack overflow https://stackoverflow.com/questions/67585809/how-to-map-the-results-of-principal-component-analysis-back-to-the-actual-featur
-    PCnames = ['PC'+str(i+1) for i in range(pca.n_components_)]
-    loadings = pd.DataFrame(pca.components_, columns=PCnames,index=feature_names)
-
-    return loadings
-
-def plot_loadings(loadings, component:str="PC1", save_dir=None):
-    # close previous plot
-    plt.close()
-
-    # see stack overflow https://stackoverflow.com/questions/67585809/how-to-map-the-results-of-principal-component-analysis-back-to-the-actual-featur
-    sns.set_theme(rc={'figure.figsize':(10, 16)})
-    loadings[component].sort_values().plot.barh()
-
-    # add more whitespace
-    plt.tight_layout(pad=2)
-
-    if save_dir: 
-        save_dir.mkdir(parents=True, exist_ok=True)
-        plt.savefig(save_dir / f"{component}.png")
+from src.utils.process_metrics import load_metrics
 
 def main(): 
     # define paths 
@@ -61,29 +19,39 @@ def main():
     # only run for TEMP 1 for now! 
     temp = 1 
 
-    # load metrics 
-    df = load_metrics(human_dir=datapath / "human_metrics", 
-                        ai_dir=datapath / "ai_metrics", temp=temp, human_completions_only=True)
+    datasets = ["stories", "dailymail_cnn", "mrpc", "dailydialog"]
 
-    # filter
-    df = filter_metrics(df, percent_NA=0.9, percent_zero=0.9, verbose=False, log_file=path.parents[0] / "pca_results" / "filtered_metrics_log.txt")
+    for dataset in datasets:
+        print(f"[INFO:] Running PCA for {dataset}...")
+        # load metrics 
+        df = load_metrics(human_dir=datapath / "human_metrics", 
+                            ai_dir=datapath / "ai_metrics", dataset=dataset, temp=temp, human_completions_only=True)
 
-    ## DEFINE FEATURES ## 
-    # get all possible features
-    all_features = df.columns.tolist() 
+        ## DEFINE FEATURES ## 
+        # get all possible features
+        all_features = df.columns.tolist() 
 
-    # define cols not to include in PCA
-    cols_to_not_include = ["model", "id", "is_human", "unique_id", "sample_params", "temperature", "prompt_number", "dataset", "annotations"]
+        # define cols not to include in PCA
+        type_cols = ["model", "id", "is_human", "unique_id", "sample_params", "temperature", "prompt_number", "dataset", "annotations"] # cols that are directly tied to type of generation
+        na_cols = ['contains_lorem ipsum', 'duplicate_line_chr_fraction', 
+                    'duplicate_ngram_chr_fraction_10', 'duplicate_ngram_chr_fraction_7', 
+                    'duplicate_ngram_chr_fraction_8', 'duplicate_ngram_chr_fraction_9', 
+                    'duplicate_paragraph_chr_fraction', 'pos_prop_SYM', 'proportion_bullet_points', 
+                    'proportion_ellipsis', 'symbol_to_word_ratio_#', 'first_order_coherence', 'second_order_coherence', 'smog'] # cols found by running identify_NA_metrics.py
 
-    # remove cols not to include from all features
-    features = [feat for feat in all_features if feat not in cols_to_not_include]
+        manually_selected_cols = ["pos_prop_SPACE", "pos_prop_PUNCT"]
 
-    # run PCA
-    pca, pca_df = run_PCA(df, feature_names=features, n_components=len(features))
+        # cols to drop
+        cols_to_drop = type_cols + na_cols + manually_selected_cols
 
-    # save PCA results
-    pca_df.to_csv(savepath / f"PCA_data_temp{temp}.csv")
+        # remove cols not to include from all features
+        features = [feat for feat in all_features if feat not in cols_to_drop]
 
+        # run PCA
+        pca, pca_df = run_PCA(df, feature_names=features, n_components=len(features))
+
+        # save PCA results
+        pca_df.to_csv(savepath / f"{dataset}_PCA_temp{temp}.csv")
 
 if __name__ == "__main__":
     main()
