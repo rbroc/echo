a32601b01105c2c15f6cd18b7fb73e0b14b2594f
MinaAlmasi <mina.almasi00@gmail.com>
2024-08-06 15:50:08 +0200

    fix: drop col if it has one NA as pca can't handle it

--- src/classify/identify_NA_metrics.py
+++ src/classify/identify_NA_metrics.py
@@ -1,5 +1,5 @@
 '''
-Identifies metrics with 90% NA or 0 values across the metrics of ALL datasets.
+Identifies metrics that contain NA and a threshold of 0 values across the metrics of ALL datasets.
 
 Since PCA cannot handle NA, we need to identify rows with NA metrics. In addition, we are also removing high percentage of 0s since they are not informative.
 Even though we are creating seperate classifiers for each dataset, we still want to compare PCA components across datasets. Therefore, the same metrics should be dropped across all datasets.
@@ -10,22 +10,22 @@ import pandas as pd
 sys.path.append(str(pathlib.Path(__file__).parents[2]))
 from src.utils.process_metrics import load_metrics
 
-def identify_NA_metrics(df, percent_NA=0.9, percent_zero=0.8):
+def identify_NA_metrics(df, percent_zero=0.8):
     '''
     Identify rows with NA metrics or high percentage of 0s
     '''
     # check if percent_NA and percent_zero are either 0, 1 or in between
-    if percent_NA < 0 or percent_NA > 1 or percent_zero < 0 or percent_zero > 1:
-        raise ValueError("percent_NA and percent_zero must be either 0 or between 0 and 1")
+    if percent_zero < 0 or percent_zero > 1:
+        raise ValueError("percent_zero must be either 0 or between 0 and 1")
 
-    identified_cols = []
+    
+    # all na_cols 
+    na_cols = df.columns[df.isna().any()].tolist()
 
     # check for NA values 
-    for col in df.columns:
-        if df[col].isna().sum() / len(df) >= percent_NA or df[col].eq(0).sum() / len(df) >= percent_zero:
-            identified_cols.append(col)
+    zero_cols = [col for col in df.columns if df[col].eq(0).sum() / len(df) >= percent_zero]
 
-    return identified_cols
+    return zero_cols + na_cols
 
 def main():
     path = pathlib.Path(__file__)
@@ -39,8 +39,12 @@ def main():
                             filter_lengths=True
                             )
 
+    # drop type cols first 
+    type_cols = ["model", "id", "is_human", "unique_id", "sample_params", "temperature", "prompt_number", "dataset"] 
+    df = df.drop(columns=type_cols)
+
     # filter metrics
-    cols = identify_NA_metrics(df, percent_NA=0.9, percent_zero=0.9)
+    cols = identify_NA_metrics(df, percent_zero=0.9)
 
     print(cols)
 
