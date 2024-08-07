9e3dbdb8334b8e8642b51d2f3d4f8c3521f84265
MinaAlmasi <mina.almasi00@gmail.com>
2024-08-07 11:31:11 +0200

    feat: add plotting functions to pca and run_pca.py, save results properly.

--- src/utils/pca.py
+++ src/utils/pca.py
@@ -52,33 +52,51 @@ def save_PCA_results(pca, results_path):
             file.write(f"pca_{i}: {variance:.8f}\n")
 
 ## loadings ## 
-def get_loadings(pca, feature_names, n_components):
+def get_loadings(pca, feature_names):
     '''
     Extract PCA loadings (for investigating which original features explain each component)
+    
+    source: stack overflow https://stackoverflow.com/questions/67585809/how-to-map-the-results-of-principal-component-analysis-back-to-the-actual-featur
     '''
-    loadings =  pca.components_.T * np.sqrt(pca.explained_variance_)
+    PCnames = ['PC'+str(i+1) for i in range(pca.n_components_)]
+    loadings = pd.DataFrame(pca.components_,columns=PCnames,index=feature_names)
 
-    column_names = [f"PC{i}" for i in range(1, n_components + 1)]
-    loadings_matrix = pd.DataFrame(loadings, index=feature_names)
+    return loadings
 
-    loadings_matrix.columns = column_names
-
-    return loadings_matrix
-
-def plot_loadings(loadings_matrix, component:int=1, outpath=None):
+def plot_loadings(loadings, component:str="PC1", save_dir=None):
     '''
-    Plot PCA loadings (for investigating which original features explain each component)
+    Plot loadings for a given component
+
+    source: stack overflow https://stackoverflow.com/questions/67585809/how-to-map-the-results-of-principal-component-analysis-back-to-the-actual-featur
     '''
-    reshaped_matrix = loadings_matrix.reset_index().melt(id_vars="index")
+    # close previous plot
+    plt.close()
 
-    colors = sns.color_palette()[0]
+    sns.set_theme(rc={'figure.figsize':(10, 16)})
+    loadings[component].sort_values().plot.barh()
 
-    plot = sns.catplot(data=reshaped_matrix[reshaped_matrix["variable"]==f"PC{component}"],
-                       x = "index", y="value", color=colors, kind='bar')
-    plt.xlim()
-    plot.set(title=f"PC{component}")
+    # add more whitespace
+    plt.tight_layout(pad=2)
 
-    plt.tight_layout()
+    if save_dir: 
+        save_dir.mkdir(parents=True, exist_ok=True)
+        plt.savefig(save_dir / f"{component}.png")
 
-    if outpath:
-        plt.savefig(outpath / f"PC_{component}.png")
+def plot_cumulative_variance(pca, title, save_dir=None, file_name=None): 
+    '''
+    plot cumulative explained variance for pca 
+
+    code by https://jakevdp.github.io/PythonDataScienceHandbook/05.09-principal-component-analysis.html 
+    '''
+    # close previous plot
+    plt.close()
+
+    sns.set_theme(rc={'figure.figsize':(10, 10)}) # see https://jakevdp.github.io/PythonDataScienceHandbook/05.09-principal-component-analysis.html 
+    plt.plot(np.cumsum(pca.explained_variance_ratio_))
+    plt.xlabel('number of components')
+    plt.ylabel('cumulative explained variance')
+    plt.title(title)
+
+    if save_dir and file_name:
+        save_dir.mkdir(parents=True, exist_ok=True)
+        plt.savefig(save_dir / file_name)
