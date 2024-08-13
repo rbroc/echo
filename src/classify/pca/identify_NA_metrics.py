6ba26f6dd7e5d3da8b93d703cd53a44fa7999c1e
MinaAlmasi <mina.almasi00@gmail.com>
2024-08-13 11:39:50 +0200

    refactor: run NA metrics without using the zero threshold, run PCA with these new (very few features)

--- src/classify/pca/identify_NA_metrics.py
+++ src/classify/pca/identify_NA_metrics.py
@@ -7,44 +7,49 @@ Even though we are creating seperate classifiers for each dataset, we still want
 import pathlib
 import sys
 import pandas as pd
-sys.path.append(str(pathlib.Path(__file__).parents[2]))
+sys.path.append(str(pathlib.Path(__file__).parents[3]))
 from src.utils.process_metrics import load_metrics
 
-def identify_NA_metrics(df, percent_zero=0.8):
+def identify_NA_metrics(df, percent_zero:float=None):
     '''
-    Identify rows with NA metrics or high percentage of 0s
-    '''
-    # check if percent_NA and percent_zero are either 0, 1 or in between
-    if percent_zero < 0 or percent_zero > 1:
-        raise ValueError("percent_zero must be either 0 or between 0 and 1")
+    Identify rows with NA metrics (and alternatively also high percentage of 0s)
 
-    
+    Args:
+        df: dataframe to check
+        percent_zero: threshold for percentage of 0s in a column to be considered for removal. Default is None (keep cols with many 0s) 
+    '''
     # all na_cols 
     na_cols = df.columns[df.isna().any()].tolist()
 
     # check for NA values 
-    zero_cols = [col for col in df.columns if df[col].eq(0).sum() / len(df) >= percent_zero]
+    if percent_zero is not None:
+        if percent_zero < 0 or percent_zero > 1: # check if percent_zero is either 0, 1 or in between 
+            raise ValueError("percent_zero must be either 0 or between 0 and 1")
 
-    return zero_cols + na_cols
+        zero_cols = [col for col in df.columns if df[col].eq(0).sum() / len(df) >= percent_zero]
+    else:
+        zero_cols = []
+
+    return na_cols + zero_cols
 
 def main():
     path = pathlib.Path(__file__)
-    datapath = path.parents[2] / "metrics"
+    datapath = path.parents[3] / "metrics"
 
     df = load_metrics(
                             human_dir=datapath / "human_metrics", 
                             ai_dir=datapath / "ai_metrics",
-                            dataset="stories", temp=1.5, 
+                            dataset=None, temp=1, 
                             human_completions_only=True,
                             filter_lengths=True
                             )
 
-    # drop type cols first 
-    type_cols = ["model", "id", "is_human", "unique_id", "sample_params", "temperature", "prompt_number", "dataset"] 
+    # drop type cols first (since they are not what should determine what features to drop)
+    type_cols = ["model", "id", "is_human", "unique_id", "sample_params", "temperature", "prompt_number", "dataset", "annotations"] 
     df = df.drop(columns=type_cols)
 
     # filter metrics
-    cols = identify_NA_metrics(df, percent_zero=0.9)
+    cols = identify_NA_metrics(df, percent_zero=None)
 
     print(cols)
 
