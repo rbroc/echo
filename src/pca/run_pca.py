b02f573ee8e4e62f35f61bfe813e54430a5fedca
MinaAlmasi <mina.almasi00@gmail.com>
2024-10-05 19:04:51 +0200

    refactor: add cols to drop in utils to simplify code

--- src/pca/run_pca.py
+++ src/pca/run_pca.py
@@ -15,6 +15,7 @@ from src.utils.pca import (
     plot_loadings,
     run_PCA,
 )
+from src.utils.cols_to_drop import get_cols_to_drop
 
 
 def main():
@@ -43,28 +44,7 @@ def main():
     all_features = train_df.columns.tolist()
 
     # cols to drop
-    type_cols = [
-        "model",
-        "id",
-        "is_human",
-        "unique_id",
-        "sample_params",
-        "temperature",
-        "prompt_number",
-        "dataset",
-        "annotations",
-    ]  #     # cols that are directly tied to type of generation (and should therefore not be included in classification)
-    na_cols = [
-        "first_order_coherence",
-        "second_order_coherence",
-        "smog",
-        "pos_prop_SPACE",
-        "per_word_perplexity"
-    ]  # cols found by running identify_NA_metrics.py
-    manually_selected_cols = [
-        "pos_prop_PUNCT"
-    ]  # no punctuation wanted (due to manipulating them)
-    cols_to_drop = type_cols + na_cols + manually_selected_cols
+    cols_to_drop = get_cols_to_drop() # see dropped cols in src/utils/cols_to_drop.py
 
     # final feature list after dropping cols
     features = [feat for feat in all_features if feat not in cols_to_drop]
