f11f4bd4ffb1b8967ac21c9b47a5da8cb68e1220
MinaAlmasi <mina.almasi00@gmail.com>
2024-10-06 22:29:02 +0200

    fix: adjust font size for plots, add black/isort formatting to script

--- src/metrics/heatmaps.py
+++ src/metrics/heatmaps.py
@@ -1,31 +1,37 @@
-'''
+"""
 create correlation heat maps 
-'''
+"""
+
 import pathlib
-import pandas as pd
-import seaborn as sns
+import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import seaborn as sns
 
-import sys
 sys.path.append(str(pathlib.Path(__file__).parents[2]))
 from src.utils.cols_to_drop import get_cols_to_drop
 
-def create_corrM(df, save_dir=None, file_name=None):
+
+def create_corrM(df, save_dir=None, file_name=None, fontsize=12, fontweight=None):
     # create correlation matrix
     corrM = df.corr()
 
     # plot
-    sns.set_theme(rc={'figure.figsize':(20, 20)})
-    # drop na from corrM 
+    sns.set_theme(rc={"figure.figsize": (20, 20)})
+    # drop na from corrM
     corrMplot = sns.heatmap(corrM.dropna(how="all"))
 
-    # tight 
+    # make labels bigger and bold
+    plt.xticks(fontsize=fontsize, fontweight=fontweight)
+    plt.yticks(fontsize=fontsize, fontweight=fontweight)
+
+    # tight
     plt.tight_layout()
 
     # save plot
-    if save_dir and file_name: 
+    if save_dir and file_name:
         save_dir.mkdir(parents=True, exist_ok=True)
         corrMplot.get_figure().savefig(save_dir / file_name)
 
@@ -35,10 +41,10 @@ def create_corrM(df, save_dir=None, file_name=None):
     return corrM, corrMplot
 
 
-def main(): 
+def main():
     path = pathlib.Path(__file__)
     datapath = path.parents[2] / "datasets_complete" / "metrics"
-    
+
     temp = 1
 
     # dirs based on temp
@@ -52,7 +58,7 @@ def main():
         / f"temp_{temp}"
         / "train_metrics.parquet"
     )
-    
+
     # filter to only include one dataset
     train_df = pd.read_parquet(datapath)
 
@@ -61,13 +67,46 @@ def main():
     cols_to_drop = get_cols_to_drop()
     features = [feat for feat in all_features if feat not in cols_to_drop]
 
-    create_corrM(train_df[features], savedir, f"heatmap_temp{temp}.png")
+    create_corrM(
+        train_df[features],
+        savedir,
+        f"heatmap_temp{temp}.png",
+        fontsize=12,
+        fontweight=None,
+    )
 
-    # subset features 
-    subset_features = ["alpha_ratio", "proportion_unique_tokens", "n_sentences", "n_unique_tokens", "doc_length", "rix", "gunning_fog", "pos_prop_NOUN", "pos_prop_NUM", "perplexity", "sentence_length_mean", "proportion_ellipsis", "pos_prop_SCONJ", "syllables_per_token_mean", "dependency_distance_mean", "pos_prop_VERB", "flesch_reading_ease", "mean_word_length", "oov_ratio", "entropy"]
+    # subset features
+    subset_features = [
+        "alpha_ratio",
+        "proportion_unique_tokens",
+        "n_sentences",
+        "n_unique_tokens",
+        "doc_length",
+        "rix",
+        "gunning_fog",
+        "pos_prop_NOUN",
+        "pos_prop_NUM",
+        "perplexity",
+        "sentence_length_mean",
+        "proportion_ellipsis",
+        "pos_prop_SCONJ",
+        "syllables_per_token_mean",
+        "dependency_distance_mean",
+        "pos_prop_VERB",
+        "flesch_reading_ease",
+        "mean_word_length",
+        "oov_ratio",
+        "entropy",
+    ]
     subset_features = sorted(subset_features)
 
-    create_corrM(train_df[subset_features], savedir, f"heatmap_temp{temp}_subset.png")
+    create_corrM(
+        train_df[subset_features],
+        savedir,
+        f"heatmap_temp{temp}_subset.png",
+        fontsize=18,
+        fontweight="bold",
+    )
 
 
 if __name__ == "__main__":
