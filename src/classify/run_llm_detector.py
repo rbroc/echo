958582bd2bad3682b782805ba0582353a5c2be31
MinaAlmasi <mina.almasi00@gmail.com>
2024-11-04 11:23:37 +0100

    feat: add few shot classifier

--- src/classify/run_llm_detector.py
+++ src/classify/run_llm_detector.py
@@ -3,7 +3,9 @@ BASELINE: Run a self-made LLM detector on the given dataset
 """
 import pandas as pd
 import argparse
+from datetime import datetime
 import pathlib
+from random import sample, seed
 
 from stormtrooper import Trooper
 from sklearn import metrics
@@ -49,6 +51,28 @@ def input_parse():
 
     return args
 
+def sample_train(train_df, sample_size:int=5, random_state:int=129):
+    """
+    Sample the training data to a smaller size for few-shot learning (stratified)
+    """
+    # split up into the two classes
+    class_0 = train_df[train_df["is_human"] == 0]
+    class_1 = train_df[train_df["is_human"] == 1]
+
+    # sample from each class
+    sample_size_class_0 = sample_size // 2
+    sample_size_class_1 = sample_size - sample_size_class_0
+
+    sample_class_0 = class_0.sample(n=sample_size_class_0, random_state=random_state)
+    sample_class_1 = class_1.sample(n=sample_size_class_1, random_state=random_state)
+
+    sample_df = pd.concat([sample_class_0, sample_class_1]).sample(frac=1, random_state=random_state)
+
+    X_train_sample = sample_df["completions"].values
+    y_train_sample = sample_df["is_human"].values
+
+    return X_train_sample, y_train_sample
+
 def main(): 
     args = input_parse()
     path = pathlib.Path(__file__)
@@ -57,10 +81,6 @@ def main():
 
     if temp == 1.0:
         temp = int(temp)
-
-    # load metrics
-    savepath = path.parents[2] / "results" / "classify" / "clf_results"
-    savepath.mkdir(parents=True, exist_ok=True)
 
     datapath = path.parents[2] / "datasets_complete" / "text" / f"temp_{temp}"
 
@@ -72,24 +92,32 @@ def main():
     val_df = val_df[val_df["dataset"] == dataset]
 
     # get correct format 
-    X_train = train_df["completions"].values
     X_val = val_df["completions"].values
-
-    y_train = train_df["is_human"].values
     y_val = val_df["is_human"].values
+
+    # sample for few-shot learning (stratified)
+    X_train_sample, y_train_sample = sample_train(train_df, sample_size=2, random_state=129)
 
     labels = [0, 1]
 
     # zero-shot classification
-    X_val = X_val[:2]
-    model = Trooper("keeeeenw/MicroLlama")
+    model_name = "google/flan-t5-small"
+    model = Trooper(model_name)
     model.fit(None, labels)
     y_pred = model.predict(X_val)
 
-    # get rapport
-    clf_report = create_eval(y_val, y_pred, val_df, savepath, f"zero_shot_clf_report")
+    savepath = path.parents[2] / "results" / "classify" / "clf_results" / "clf_reports" / f"{dataset}_temp{temp}"
+    savepath.mkdir(parents=True, exist_ok=True)
+    clf_report = create_eval(y_val, y_pred, val_df, savepath, f"zero_shot_{model_name.split('/')[1]}")
 
-    
+    # few-shot classification
+    model = Trooper(model_name)
+    model.fit(X_train_sample, y_train_sample)
+    y_pred = model.predict(X_val)
+
+    savepath = path.parents[2] / "results" / "classify" / "clf_results" / "clf_reports" / f"{dataset}_temp{temp}"
+    savepath.mkdir(parents=True, exist_ok=True)
+    clf_report = create_eval(y_val, y_pred, val_df, savepath, f"few_shot_{model_name.split('/')[1]}")
 
 if __name__ == "__main__":
     main()
